7d7d521e1244ea1582a2940c3672af99edfe0c9e
Mr. Marco Prins <snirpocram@gmail.com>
2021-02-24 21:18:11 +0200

    Minor restructure of frontend directory

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Mobility Intelligence Code Challenge
+          Mobility Intelligence Code Challunges
         </p>
-        
       </header>
     </div>
   );
