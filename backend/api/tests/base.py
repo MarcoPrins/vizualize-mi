ead2da174c45a503b517731f39de0e32eaa9e786
Mr. Marco Prins <snirpocram@gmail.com>
2021-03-02 20:26:52 +0200

    Drop and recreate tables in test setup method

--- backend/api/tests/base.py
+++ backend/api/tests/base.py
@@ -7,10 +7,16 @@ from flask_testing import TestCase
 from helpers.database import execute_multi_sql
 from api.tests import app
 
+create_tables = os.path.join(app.config['PROJECT_ROOT'], 'fixtures/create_tables.sql')
+drop_tables = os.path.join(app.config['PROJECT_ROOT'], 'fixtures/drop_tables.sql')
 
 class BaseTestCase(TestCase):
+    render_templates = False
+
     def create_app(self):
-        create_tables = os.path.join(app.config['PROJECT_ROOT'], 'fixtures/create_tables.sql')
+        """ App is imported from the api package to ensure similar setup """
+        with app.app_context():
+          execute_multi_sql(open(drop_tables).read())
         with app.app_context():
           execute_multi_sql(open(create_tables).read())
         return app
