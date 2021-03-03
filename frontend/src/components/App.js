21034e33c17204041357f63c2daa3e5c2eca3fba
Mr. Marco Prins <snirpocram@gmail.com>
2021-03-03 20:39:31 +0200

    Add simulation dashboard

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,31 @@
+import { Fragment } from 'react';
+import SimulationDashboard from './SimulationDashboard';
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Mobility Intelligence Code Challenge
-        </p>
-      </header>
-    </div>
+    <Fragment>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <span class="navbar-text">
+                Simulate a ridepooling service!
+              </span>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="App">
+        <div class="container">
+          <div class="row gy-5">
+            <div class="col-sm">
+              <SimulationDashboard />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ frontend/src/components/SimulationDashboard.js
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import axios from "axios";
+
+import SimulationPreview from './SimulationPreview';
+
+class SimulationDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.triggerSimulation = this.triggerSimulation.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+
+    this.state = {
+      region_id: 'de_berlin',
+      number_of_requests: 1,
+      simulation_results: null,
+    }
+  }
+
+  triggerSimulation() {
+    const { region_id, number_of_requests } = this.state;
+
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/simulations`, {
+      region_id,
+      number_of_requests,
+    })
+      .then((response) => {
+        this.setState({
+          simulation_results: response,
+        })
+      })
+      .catch(() => {
+        // TODO: Proper error handling
+        console.log('failed');
+      });
+  }
+
+  handleUpdate(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    const { simulation_results, region_id, number_of_requests } = this.state;
+
+    return(
+      <div>
+        <form>
+          <div class="mb-3">
+            <label for="number_of_requests" class="form-label">Number of requests</label>
+            <input name="number_of_requests" value={number_of_requests} type="number" class="form-control" onChange={this.handleUpdate} />
+          </div>
+
+          <div class="mb-3">
+            <label for="region_id" class="form-label">Region ID</label>
+            <input name="region_id" value={region_id} type="text" class="form-control" onChange={this.handleUpdate} />
+          </div>
+
+          <button className="btn btn-primary" onClick={this.triggerSimulation}>
+            Trigger Simulation
+          </button>
+        </form>
+
+
+        {simulation_results &&
+          <div>
+            <SimulationPreview data={simulation_results} />
+          </div>}
+      </div>
+    );
+  }
+}
+
+export default SimulationDashboard;
